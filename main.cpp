5d0130eb1d8accde1376952a9f08e1f379c076ee
MiBeloz <miha_bmv_@mail.ru>
2023-02-11 23:18:55 +0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#define SUB(a, b) ((a) - (b))
+
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL, "ru");
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+	system("pause > nul");
+
+	return 0;
+}
